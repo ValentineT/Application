6d509a73cb8481e634f707df6f78b70fb29e40e3
valentineT <valentine.timinsky@gmail.com>
2018-04-04 03:17:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/valentine/myapplication/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.valentine.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private TextView messageText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = this.getIntent();
+        messageText = findViewById(R.id.messageText);
+        messageText.setText(intent.getStringExtra(getString(R.string.key_message)));
+    }
+}
